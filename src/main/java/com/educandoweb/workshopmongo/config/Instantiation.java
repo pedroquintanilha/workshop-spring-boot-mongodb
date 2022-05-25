830614894366ce8a991c41c39b9fc642f2a92e75
Pedro Quintanilha <pedrosquinta@gmail.com>
2022-05-25 18:32:01 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -1,5 +1,7 @@
 package com.educandoweb.workshopmongo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +13,19 @@ import com.educandoweb.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		
-		
-		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	
 		
 	}
 
