94a1d74ba4554a8112351e99c07727d8b4da3896
Pedro Quintanilha <pedrosquinta@gmail.com>
2022-06-22 17:48:31 -0300

    Consulta com varios criterios

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -65,7 +65,7 @@ public class UserResource {
 	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
 		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj.getPosts());	
+		return ResponseEntity.ok().body(obj.getPosts());	 
 	}    
 	
 }
